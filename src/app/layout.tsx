b36cab362767990252e88202f1402402fec29992
ahoang2502 <athoang252@gmail.com>
2024-03-29 13:44:48 +1100

    style: new fonts added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,17 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Inria_Serif } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+const inria = Inria_Serif({
+  subsets: ["latin", "latin-ext"],
+  weight: ["300", "400", "700"],
+  style: ["normal", "italic"],
+  variable: "--font-inria",
+});
 
 export const metadata: Metadata = {
   title: "Medium",
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,12 @@ export default function Home() {
   return (
     <main>
       <Navbar />
+
+      <p className="">hello</p>
+      <p className="font-inria text-normal">helloosnsj </p>
+      <p className="font-sourceSerif font">hello</p>
+      <p className="font-sourceSerifLight">hello</p>
+      <p className="font-inriaBold text-normal">hello</p>
     </main>
   );
 }
