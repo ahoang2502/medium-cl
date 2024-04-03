a34ce7bf87161c0645d82e69987495f81825c4c0
ahoang2502 <athoang252@gmail.com>
2024-04-03 17:41:19 +1100

    feat: publish story

---
+++ src/app/api/publish-new-story/route.ts
@@ -0,0 +1,32 @@
+import { NextRequest, NextResponse } from "next/server";
+
+import { db } from "@/app/prismadb";
+
+export async function PATCH(req: NextRequest) {
+  const { storyId, topics } = await req.json();
+  if (!storyId) throw new Error("Missing Story ID");
+
+  const story = await db.story.findUnique({
+    where: {
+      id: storyId,
+    },
+  });
+  if (!story) throw new Error("No story found");
+
+  try {
+    const updatedStory = await db.story.update({
+      where: {
+        id: story.id,
+      },
+      data: {
+        publish: true,
+        topics,
+      },
+    });
+
+    return NextResponse.json(updatedStory);
+  } catch (error) {
+    console.log("🔴 PATCH update_story ", error);
+    return NextResponse.error();
+  }
+}
---
+++ src/app/published/[storyId]/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const PublishedStoryDetailsPage = ({
+  params,
+}: {
+  params: {
+    storyId: string;
+  };
+}) => {
+  return <div>
+    helo
+  </div>;
+};
+
+export default PublishedStoryDetailsPage;
