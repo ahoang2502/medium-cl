728d8eb05f91eb1dcac38deca0851a1bffdd2e19
ahoang2502 <athoang252@gmail.com>
2024-04-06 22:28:30 +1100

    feat:story-details page: share functionality

--- src/app/published/[storyId]/_components/ShareComponent.tsx
+++ src/app/published/[storyId]/_components/ShareComponent.tsx
@@ -1,7 +1,38 @@
-import React from 'react'
+"use client";
+
+import { ShareIcon } from "@/components/icons/ShareIcon";
+import { usePathname } from "next/navigation";
+import React, { useState } from "react";
 
 export const ShareComponent = () => {
+  const [showPopup, setShowPopup] = useState<boolean>(false);
+  const pathname = usePathname();
+
   return (
-    <div>ShareComponent</div>
-  )
-}
+    <div className="relative">
+      <button className="" onClick={() => setShowPopup(!showPopup)}>
+        <ShareIcon />
+      </button>
+
+      {showPopup && (
+        <div className="w-[250px] h-[100px] rounded-md shadow-lg absolute bottom-12 -left-20 ">
+          <input
+            className="overflow-hidden p-2 m-2 border-[1px] rounded-md bg-gray-100 w-full"
+            value={`${process.env.NEXT_PUBLIC_URL}${pathname}`}
+          />
+
+          <button
+            onClick={() =>
+              navigator.clipboard.writeText(
+                `${process.env.NEXT_PUBLIC_URL}${pathname}`
+              )
+            }
+            className="text-center w-full py-2"
+          >
+            Copy link
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
